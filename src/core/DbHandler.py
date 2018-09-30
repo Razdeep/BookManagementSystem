8e2fbabe62cdc3c58d9e4a223039bc5000cd210f
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2018-09-30 17:53:50 +0530

    loginService.py added

--- src/core/DbHandler.py
+++ src/core/DbHandler.py
@@ -3,10 +3,14 @@ Created on Sep 29, 2018
 @author: Rajdeep Roy Chowdhury
 Licensed Under MIT
 '''
+
+# This class must implemented if concurrent connections fail
+
 import sqlite3
 class DbHandler:
     def __init__(self):
         self.conn=sqlite3.connect('data.db')
+    def getConnection(): pass
 
 
 # Driver code
